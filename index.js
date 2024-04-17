d05ff80fd734a33323c89db1b8bd58b92811456c
Marvin Kabira <marvobd@gmail.com>
2024-04-17 21:15:32 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const leastFavoriteCustomer = 'jane';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'richard';
+}
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+};
+
+bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+};
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+
+
